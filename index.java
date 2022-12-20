40a4a1ec72994714642f905c4203702e5f1ed469
Emmanuel A <emmanuelxxix@gmail.com>
2022-12-20 15:48:38 +0200

    Update index.java

--- index.java
+++ index.java
@@ -7,10 +7,7 @@ public class AsciiValue {
         
         char ch = input;
         int ascii = ch;
-        // You can also cast char to int
-        int castAscii = (int) ch;
 
         System.out.println("The ASCII value of " + ch + " is: " + ascii);
-        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
     }
 }
